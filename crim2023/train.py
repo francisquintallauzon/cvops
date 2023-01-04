29af01f779623baf784ccbccf23c8ed83cbb6e3d
francis <francisquintallauzon@gmail.com>
2023-01-04 17:50:51 -0500

    MLOps103 - Prep de l'env de dev

---
+++ crim2023/train.py
@@ -0,0 +1,13 @@
+import pytorch_lightning as pl
+from crim2023.model import CellModel
+from crim2023.data import CellDataModule
+
+
+def train_model():
+    model = CellModel(init_features=1)
+    dm = CellDataModule(batch_size = 1, max_images = 5, num_workers=0)
+    trainer = pl.Trainer(max_epochs=5, log_every_n_steps=1)
+    trainer.fit(model, dm)
+
+if __name__ == '__main__':
+    train_model()
