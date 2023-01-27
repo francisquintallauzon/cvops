b360e78816083eb8629b5b5c2e8bf937f277aa86
francisPLT <francis.pelletier@crim.ca>
2023-01-26 19:05:20 -0500

    Remove super() arguments for UNet class
    These arguments were flagged by IDE. Upon reading documentation
    they seemed redundant, and didn't seem to impact behaviour

--- cvops/model.py
+++ cvops/model.py
@@ -6,7 +6,7 @@ from torch import nn
 
 class UNet(nn.Module):
     def __init__(self, in_channels=3, out_channels=1, init_features=32):
-        super(UNet, self).__init__()
+        super().__init__()
 
         features = init_features
         self.encoder1 = UNet._block(in_channels, features, name="enc1")
